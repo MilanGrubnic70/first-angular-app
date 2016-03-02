57824bfc8b2d289d8d236348c913a9a8e05a3884
Milan Grubnić <MilanG@aol.com>
2016-03-01 21:31:19 -0600

    added additional books

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,15 +1,30 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = "Top Sellers in Books",
         $scope.promo = "Milan's Collection",
-        $scope.products = [{
-            name: "The Book of Trees",
-            price: 19,
-            pubdate: new Date('2014', '03', '08'),
-            cover: 'img/the-book-of-trees.jpg'
-        }, {
-            name: "Program or be Programmed",
-            price: 8,
-            pubdate: new Date('2013', '08', '01'),
-            cover: 'img/program-or-be-programmed.jpg'
-        }]
+        $scope.products = [
+            {
+                name: "The Book of Trees",
+                price: 19,
+                pubdate: new Date('2014', '03', '08'),
+                cover: 'img/the-book-of-trees.jpg'
+            },
+            {
+                name: "Program or be Programmed",
+                price: 8,
+                pubdate: new Date('2013', '08', '01'),
+                cover: 'img/program-or-be-programmed.jpg'
+            },
+            {
+                name: "Programmed Programmer",
+                price: 18,
+                pubdate: new Date('2013', '08', '01'),
+                cover: 'img/program-or-be-programmed.jpg'
+            },
+            {
+                name: "Tree Book",
+                price: 29,
+                pubdate: new Date('2014', '03', '08'),
+                cover: 'img/the-book-of-trees.jpg'
+            }
+        ]
 }]);
