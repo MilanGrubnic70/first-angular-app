f3d6d2f6a8d5fc579465531d13d36d9837114065
Milan Grubnić <MilanG@aol.com>
2016-03-01 20:46:01 -0600

    replaced product property with products array and added repeat directive in view

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,20 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = "Top Sellers in Books",
         $scope.promo = "Milan's Collection",
-        $scope.product = {
-            name: "The Book of Trees",
-            price: 19,
-            pubdate: new Date('2014', '03', '08') 
-        }
+        $scope.products = [
+
+
+            {
+                name: "The Book of Trees",
+                price: 19,
+                pubdate: new Date('2014', '03', '08')
+            },
+
+            {
+                name: "Program or be Programmed",
+                price: 8,
+                pubdate: new Date('2013', '08', '01')
+            }
+
+        ]
 }]);
