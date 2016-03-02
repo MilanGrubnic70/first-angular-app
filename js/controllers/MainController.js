e4aef1c4433b73f189db0e026edfa56604b6e0ce
Milan Grubnić <MilanG@aol.com>
2016-03-01 20:36:16 -0600

    refactored

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,6 +4,6 @@ app.controller('MainController', ['$scope', function($scope) {
         $scope.product = {
             name: "The Book of Trees",
             price: 19,
-            pubdate: new Date('2014', '03', '08')
+            pubdate: new Date('2014', '03', '08') 
         }
 }]);
