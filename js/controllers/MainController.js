6c9ccb2f9bd7057528cafbfbd0de8e827b11399d
Milan Grubnić <MilanG@aol.com>
2016-03-01 21:25:52 -0600

    added book cover property and expression

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,15 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = "Top Sellers in Books",
         $scope.promo = "Milan's Collection",
-        $scope.products = [
-
-
-            {
-                name: "The Book of Trees",
-                price: 19,
-                pubdate: new Date('2014', '03', '08')
-            },
-
-            {
-                name: "Program or be Programmed",
-                price: 8,
-                pubdate: new Date('2013', '08', '01')
-            }
-
-        ]
+        $scope.products = [{
+            name: "The Book of Trees",
+            price: 19,
+            pubdate: new Date('2014', '03', '08'),
+            cover: 'img/the-book-of-trees.jpg'
+        }, {
+            name: "Program or be Programmed",
+            price: 8,
+            pubdate: new Date('2013', '08', '01'),
+            cover: 'img/program-or-be-programmed.jpg'
+        }]
 }]);
